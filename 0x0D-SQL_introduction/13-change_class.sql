1e0e6b891ed7563f2ab94d456ecaac0937e2a222
Mallam1234 <mallamibrahimrib19@gmail.com>
2023-05-20 09:34:19 -0700

    tasks13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- Update the score of Bob to 10 in the table second_table
-UPDATE second_table SET score = 10 WHERE name = 'Bob';
+-- Remove all records with a score <= 5 in the table 'second_table'
+DELETE FROM second_table WHERE score <= 5;
