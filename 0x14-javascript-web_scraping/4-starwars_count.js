ec9ec52d2f7cf5ee2f38109f16387e9427b422f1
olaoye ibrahim <youngalhaji2000@gmail.com>
2023-08-10 19:36:57 -0700

    tasks5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,13 +4,15 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 
+const wedgeAntillesId = 18;
+
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const data = JSON.parse(body);
     const moviesWithWedge = data.results.filter(movie => {
-      return movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+      return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`);
     });
 
     console.log(moviesWithWedge.length);
